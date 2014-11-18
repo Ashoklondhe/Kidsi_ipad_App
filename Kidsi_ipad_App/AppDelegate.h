c15bcec2068a922b4c55d2f3b4882209e2c7e850
Ashok <ashok@Ashoks-Mac-Pro.local>
2014-11-18 17:25:50 +0600

    Initial Commit

---
+++ Kidsi_ipad_App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Kidsi_ipad_App
+//
+//  Created by Ashok on 11/18/14.
+//  Copyright (c) 2014 MDC Concepts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
